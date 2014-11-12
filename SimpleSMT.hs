fac4b9896dd15617c2710245837dc34c1c3dc7b2
Iavor S. Diatchki <diatchki@galois.com>
2014-11-12 15:48:44 -0800

    Add support for declarations, and more theories.

--- SimpleSMT.hs
+++ SimpleSMT.hs
@@ -13,7 +13,8 @@ import qualified Control.Exception as X
 import Control.Concurrent(forkIO)
 import Control.Monad(forever)
 import Text.Read(readMaybe)
-import Data.Ratio((%))
+import Data.Ratio((%), numerator, denominator)
+import Numeric(readHex)
 
 
 -- | Results of checking for satisfiability.
@@ -21,8 +22,11 @@ data SmtRes = Unsat | Unknown | Sat
               deriving (Eq,Show)
 
 -- | Common values returned by SMT solvers.
-data SmtVal = SmtInt !Integer | SmtReal !Rational | SmtBool Bool
-            | SmtOther SExpr
+data SmtVal = SmtInt !Integer
+            | SmtReal !Rational
+            | SmtBool !Bool
+            | SmtBits !Int !Integer   -- ^ width, value
+            | SmtOther !SExpr
               deriving (Eq,Show)
 
 -- | S-expressions, which are used in SMTLIB-2.
@@ -143,6 +147,10 @@ solverPush proc = solverSimpleCmd proc [ "push", "1" ]
 solverPop :: SolverProcess -> IO ()
 solverPop proc = solverSimpleCmd proc [ "pop", "1" ]
 
+solverDeclareFun :: SolverProcess -> String -> [SExpr] -> SExpr -> IO ()
+solverDeclareFun proc f as r =
+  solverAckCmd proc $ smtFun "declare-fun" [ SAtom f, SList as, r ]
+
 -- | Assume a fact
 solverAssume :: SolverProcess -> SExpr -> IO ()
 solverAssume proc e = solverAckCmd proc $ SList [ SAtom "assert", e ]
@@ -167,6 +175,10 @@ sexprToVal expr =
   case expr of
     SAtom "true"                    -> SmtBool True
     SAtom "false"                   -> SmtBool False
+    SAtom ('#' : 'b' : ds)
+      | Just n <- binLit ds         -> SmtBits (length ds) n
+    SAtom ('#' : 'x' : ds)
+      | [(n,[])] <- readHex ds      -> SmtBits (4 * length ds) n
     SAtom txt
       | Just n <- readMaybe txt     -> SmtInt n
     SList [ SAtom "-", x ]
@@ -176,6 +188,13 @@ sexprToVal expr =
       , SmtInt b <- sexprToVal y    -> SmtReal (a % b)
     _ -> SmtOther expr
 
+  where
+  binLit cs = do ds <- mapM binDigit cs
+                 return $ sum $ zipWith (*) (reverse ds) powers2
+  powers2   = 1 : map (2 *) powers2
+  binDigit '0' = Just 0
+  binDigit '1' = Just 1
+  binDigit _   = Nothing
 
 -- | Get the values of some s-expressions.
 -- Only valid after a 'Sat' result.
@@ -195,6 +214,160 @@ solverGetExprs proc vals =
 solverGetVars :: SolverProcess -> [String] -> IO [SmtVal]
 solverGetVars proc xs = solverGetExprs proc (map SAtom xs)
 
+
+--------------------------------------------------------------------------------
+
+
+smtFam :: String -> [Integer] -> SExpr
+smtFam f is = SList (SAtom "_" : SAtom f : map (SAtom . show) is)
+
+smtFun :: String -> [SExpr] -> SExpr
+smtFun f [] = SAtom f
+smtFun f as = SList (SAtom f : as)
+
+smtConst :: String -> SExpr
+smtConst f = smtFun f []
+
+
+stmIntT :: SExpr
+stmIntT = smtConst "Int"
+
+stmBoolT :: SExpr
+stmBoolT = smtConst "Bool"
+
+smtRealT :: SExpr
+smtRealT = smtConst "Real"
+
+smtArrayT :: SExpr -> SExpr -> SExpr
+smtArrayT x y = smtFun "Array" [x,y]
+
+smtBitsT :: Integer -> SExpr
+smtBitsT w = smtFam "BitVec" [w]
+
+
+smtTrue :: SExpr
+smtTrue = smtConst "true"
+
+smtFalse :: SExpr
+smtFalse = smtConst "false"
+
+smtNot :: SExpr -> SExpr
+smtNot p = smtFun "not" [p]
+
+smtAnd :: SExpr -> SExpr -> SExpr
+smtAnd p q = smtFun "and" [p,q]
+
+smtOr :: SExpr -> SExpr -> SExpr
+smtOr p q = smtFun "or" [p,q]
+
+smtXor :: SExpr -> SExpr -> SExpr
+smtXor p q = smtFun "xor" [p,q]
+
+smtImplies :: SExpr -> SExpr -> SExpr
+smtImplies p q = smtFun "=>" [p,q]
+
+
+
+smtITE :: SExpr -> SExpr -> SExpr -> SExpr
+smtITE x y z = smtFun "ite" [x,y,z]
+
+smtEq :: SExpr -> SExpr -> SExpr
+smtEq x y = smtFun "=" [x,y]
+
+
+smtInt :: Integer -> SExpr
+smtInt x | x < 0     = smtNeg (smtInt (negate x))
+         | otherwise = SAtom (show x)
+
+smtReal :: Rational -> SExpr
+smtReal x = smtRealDiv (smtInt (denominator x)) (smtInt (numerator x))
+
+smtGt :: SExpr -> SExpr -> SExpr
+smtGt x y = smtFun ">" [x,y]
+
+smtLt :: SExpr -> SExpr -> SExpr
+smtLt x y = smtFun "<" [x,y]
+
+smtGeq :: SExpr -> SExpr -> SExpr
+smtGeq x y = smtFun "<=" [x,y]
+
+smtLeq :: SExpr -> SExpr -> SExpr
+smtLeq x y = smtFun "<=" [x,y]
+
+
+
+smtAdd :: SExpr -> SExpr -> SExpr
+smtAdd x y = smtFun "+" [x,y]
+
+smtSub :: SExpr -> SExpr -> SExpr
+smtSub x y = smtFun "-" [x,y]
+
+smtNeg :: SExpr -> SExpr
+smtNeg x = smtFun "-" [x]
+
+smtMul :: SExpr -> SExpr -> SExpr
+smtMul x y = smtFun "*" [x,y]
+
+smtAbs :: SExpr -> SExpr
+smtAbs x = smtFun "abs" [x]
+
+smtDiv :: SExpr -> SExpr -> SExpr
+smtDiv x y = smtFun "div" [x,y]
+
+smtMod :: SExpr -> SExpr -> SExpr
+smtMod x y = smtFun "mod" [x,y]
+
+smtDivisible :: SExpr -> Integer -> SExpr
+smtDivisible x n = SList [ smtFam "divisible" [n], x ]
+
+smtRealDiv :: SExpr -> SExpr -> SExpr
+smtRealDiv x y = smtFun "/" [x,y]
+
+
+smtConcat :: SExpr -> SExpr -> SExpr
+smtConcat x y = smtFun "concat" [x,y]
+
+smtExtract :: SExpr -> Integer -> Integer -> SExpr
+smtExtract x y z = SList [ smtFam "extract" [y,z], x ]
+
+smtBvNot :: SExpr -> SExpr
+smtBvNot x = smtFun "bvnot" [x]
+
+smtBvNeg :: SExpr -> SExpr
+smtBvNeg x = smtFun "bvneg" [x]
+
+smtBvAnd :: SExpr -> SExpr -> SExpr
+smtBvAnd x y = smtFun "bvand" [x,y]
+
+smtBvOr :: SExpr -> SExpr -> SExpr
+smtBvOr x y = smtFun "bvor" [x,y]
+
+smtBvAdd :: SExpr -> SExpr -> SExpr
+smtBvAdd x y = smtFun "bvadd" [x,y]
+
+smtBvMul :: SExpr -> SExpr -> SExpr
+smtBvMul x y = smtFun "bvmul" [x,y]
+
+smtBvUDiv :: SExpr -> SExpr -> SExpr
+smtBvUDiv x y = smtFun "bvudiv" [x,y]
+
+smtBvURem :: SExpr -> SExpr -> SExpr
+smtBvURem x y = smtFun "bvurem" [x,y]
+
+smtBvShl :: SExpr -> SExpr -> SExpr
+smtBvShl x y = smtFun "bvshl" [x,y]
+
+smtBvLShr :: SExpr -> SExpr -> SExpr
+smtBvLShr x y = smtFun "bvshr" [x,y]
+
+smtBvULt :: SExpr -> SExpr -> SExpr
+smtBvULt x y = smtFun "bvult" [x,y]
+
+smtSelect :: SExpr -> SExpr -> SExpr
+smtSelect x y = smtFun "select" [x,y]
+
+smtStore :: SExpr -> SExpr -> SExpr -> SExpr
+smtStore x y z = smtFun "store" [x,y,z]
 
 
 
