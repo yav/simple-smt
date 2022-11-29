4e26627db7905aa8aa0baed0c9563c5d0b6b363b
Quentin Aristote <quentin@aristote.fr>
2022-11-29 11:57:35 +0100

    tests: expose reusable parts

---
+++ tests/SimpleSMT/Tests.hs
@@ -0,0 +1,8 @@
+module SimpleSMT.Tests
+  ( testBackend,
+    Source(..),
+    sources
+  ) where
+
+import SimpleSMT.Tests.Solver (testBackend)
+import SimpleSMT.Tests.Sources (Source(..), sources)
